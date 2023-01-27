63691288cedfe88ff961d953a0aeb67c75eef29c
Eric B <ericbieszczad@gmail.com>
2023-01-27 22:14:58 +0100

    Fixed sieve

--- super-optimised/main.cpp
+++ super-optimised/main.cpp
@@ -5,17 +5,15 @@
 #include <cmath>
 
 using namespace std::chrono;
+using namespace std;
 
-using namespace::std;
-
-vector<int> find_primes_up_to(int n);
-bool is_prime(int n);
+vector<long> find_primes_up_to(long cutoff);
 
 int main() {
-    int N = 1'000'000;
+    long N = 10'000'000;
 
     auto start = high_resolution_clock::now();
-    vector<int> found_primes = find_primes_up_to(N);
+    vector<long> found_primes = find_primes_up_to(N);
     auto stop = high_resolution_clock::now();
 
     cout << "Found " << found_primes.size() << " prime numbers up to " << N << " ";
@@ -23,54 +21,28 @@ int main() {
     return 0;
 }
 
-vector<int> find_primes_up_to(int n) {
+vector<long> find_primes_up_to(long cutoff) {
+    // Create a boolean sieve vector containing only every odd n from 3 and up to cutoff
+    vector<bool> boolean_sieve = vector<bool>(cutoff + 1, true);
+    boolean_sieve[0] = false;
+    boolean_sieve[1] = false;
 
-    // Create a boolean sieve vector containing only every odd number from 3 and up to n
-    vector<bool> boolean_sieve = vector<bool>(n/2 + (n % 2 == 1 ? 1 : 0), true);
-
-    for (int i = 0; i < boolean_sieve.size(); i++) {
+    for (long i = 2; i <= cutoff; i++) {
         if (!boolean_sieve[i]) continue;
 
-        // Example: index 0 is 3, index 1 is 5
-        // number = (index+1)*2 + 1
-        // The "real" value of the index we're at:
-        int number = (i+1)*2 + 1;
-        if (is_prime(number)) continue;
-        boolean_sieve[i] = false;
-
-        // Turn all future factors of n into false
-        // Watch out for integer overflows
-        if (number >= 65536) continue;
-        for (int x = number * number; number <= n; number += number) {
-            // We need to calculate the index from the value n.
-            // Remember that index 0 is 3, index 1 is 5 etc...
-            int calculated_index = number / 2 - 1;
-            boolean_sieve[calculated_index] = false;
+        for (long future_index = i*i; future_index <= cutoff; future_index += i) {
+            boolean_sieve[future_index] = false;
         }
     }
 
-    // Convert the indicies of the true values into ints
-    vector<int> primes = vector<int>();
-    primes.push_back(2);
+    // Convert from indices to numbers
+    vector<long> primes = vector<long>();
 
-    for (int i = 0; i < boolean_sieve.size(); i++) {
-        if (!boolean_sieve[i]) continue;
-        primes.push_back((i+1)*2 + 1);
+    for (long i = 0; i <= cutoff; i++) {
+        if (boolean_sieve[i]) {
+            primes.push_back(i);
+        }
     }
 
-    // return the primes!
     return primes;
-}
-
-// TODO: Only check if the number is divisble by any previous
-// primes found in the prime sieve.
-bool is_prime(int n) {
-    if (n < 2) return false;
-    for (int i = 3; i < sqrt(n); i += 2) {
-        if (n % i == 0) return false;
-
-        // Watch out for integer overflows. Next run will i*i will cause an overflow
-        // if (i+1 >= 65536) break;
-    }
-    return true;
 }
